53232702e9ee04d7e23e180f9d6daff71ee2d8cc
abfiker <new94abemelek@gmail.com>
2024-05-20 15:29:46 +0300

    header and footer components are added

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseonselect={true.toString()} >
+                <Container>
+                    <Navbar.Brand href='/'>
+                        <img src={logo} alt="proshop image" />
+                        ProShop
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='/cart'>
+                                <FaShoppingCart /> Cart
+                            </Nav.Link>
+                            <Nav.Link href='/login'>
+                                <FaUser /> Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
