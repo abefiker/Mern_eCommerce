df806f37b6734d574460337ebd1533ea5a98fe83
abfiker <new94abemelek@gmail.com>
2024-05-21 13:24:23 +0300

    error handling added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,17 +4,24 @@ import Product from '../models/productModel.js';
 import asyncHandler from '../middleware/asyncHandler.js';
 
 router.get('/', asyncHandler(async (req, res) => {
-
     const products = await Product.find({});
-    res.json(products)
+    if (products) {
+        res.json(products);
+    } else {
+        res.status(404)
+        throw new Error('Resource not found')
+    }
 }));
+
 router.get('/:id', asyncHandler(async (req, res) => {
 
     const product = await Product.findById(req.params.id)
     if (product) {
         res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Resource not found')
     }
-    res.status(404).json({message:'Product not found'})
-}))
+}));
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js';
 import express from 'express';
 const port = process.env.PORT
 import productRoutes from './routes/productRoutes.js';
-
+import { notFound , errorHandler } from './middleware/errorMiddleware.js';
 connectDB();
 const app = express();
 
@@ -12,6 +12,9 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
