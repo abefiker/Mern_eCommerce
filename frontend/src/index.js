628a6580385bcf2568620e244e8645b371d12878
abfiker <new94abemelek@gmail.com>
2024-05-22 22:27:58 +0300

    redux setup and apiSlices

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css';
 
-import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
@@ -12,9 +16,9 @@ import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index={true} path='/' element={<HomeScreen/>}/>
-      <Route path='/product/:id' element={<ProductScreen/>}/>
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
@@ -22,7 +26,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
